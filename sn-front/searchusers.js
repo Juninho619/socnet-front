d057ac297e86b4fbaa4738613ced0ea1b6808359
Juninho619 <christophebontemps619@gmail.com>
2024-05-23 10:36:40 +0200

    follow

--- sn-front/searchusers.js
+++ sn-front/searchusers.js
@@ -8,8 +8,33 @@ async function showAllUsers() {
   console.log(users);
   console.log(response);
 
+  // follow function
+  // select button
+  // implement search here as well
+
   users.forEach((element) => {
     container.innerHTML += `<div class="container mx-auto border-gray-500 rounded-md m-4 p-4"<img src="${element.user_profile}"/><h2>${element.username}</h2><p>${element.user_email}</p><p>${element.first_name}</p><p>${element.last_name}<button class="follow border-1 border-black bg-blue-400 text-white ml-2 rounded-md p-2"">Follow</button></div>`;
+    let followButton = document.querySelector(".follow");
+
+    followButton.addEventListener("click", async () => {
+      let followRequest = {
+        follower_id: followerId,
+        followed_id: followedid,
+      };
+
+      let followUser = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(followRequest),
+      };
+      let publishComment = await fetch(
+        "http://localhost:3045/follow",
+        followUser
+      );
+      let response = await publishComment.json();
+    });
   });
 }
 
